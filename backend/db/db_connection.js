9479c26da8615dedf8c8cba12001b7a2e18e658d
akashhowly <yourhowlyno1@gmail.com>
2025-03-24 08:59:24 +0530

    howly

--- backend/db/db_connection.js
+++ backend/db/db_connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '765876',
+    password: '@mysql',
     database: 'inventory_db',
     waitForConnections: true,
     connectionLimit: 10,
